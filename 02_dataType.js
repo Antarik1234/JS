e225e81dd8e20648879212abd3fa72d7dba32130
Antarik Sinha <120300062+Antarik1234@users.noreply.github.com>
2025-01-07 15:22:09 +0000

    lec2

---
+++ 02_dataType.js
@@ -0,0 +1,17 @@
+"use strict"; //use newer version of javascript
+
+//alert("hi")  //cannot use in nodejs, used in browser
+
+let name="Abc"
+let age = 15
+let isMale = true
+
+// numbers => upto 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone
+// undefined => uniqness
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
