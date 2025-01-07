5ac8af057e95232872caa0bfdd8082654079bdd3
Antarik Sinha <120300062+Antarik1234@users.noreply.github.com>
2025-01-07 15:02:33 +0000

    lec1

---
+++ 01_var.js
@@ -0,0 +1,10 @@
+const name = "Antarik"
+let age = 20
+var phno="8240******"
+city ="kol"
+let job
+//name = "Sayak" cannot be changed as it is declared as constant
+age = "21"
+phno = "9831******"  //is not used as it has issue in scope
+city = "ben"    //never used this 
+console.table([name,age,phno,city,job]) 
