64f2f7a1c08fb3def1924f16410ce5ede3a4c574
Antarik Sinha <120300062+Antarik1234@users.noreply.github.com>
2025-01-08 17:24:16 +0000

    lec9

--- 01_var.js
+++ 01_var.js
@@ -8,3 +8,17 @@ age = "21"
 phno = "9831******"  //is not used as it has issue in scope
 city = "ben"    //never used this 
 console.table([name,age,phno,city,job]) 
+
+// primitive datatypes : number , string , null , undefined , symbol , bigint(number with n at end) , boolean
+
+// reference : array , objects , functions
+const array =["a","b","c"];
+let isObject={
+    rank : 50,
+    clg : "ju",
+}
+let isFunc = function(){
+    console.log("hello");
+    
+}
+console.log(typeof 123498320843797797789798797);
