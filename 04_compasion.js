9689845768895132fa00a219ba1b34aead40f146
Antarik Sinha <120300062+Antarik1234@users.noreply.github.com>
2025-01-08 15:19:07 +0000

    lec4

---
+++ 04_compasion.js
@@ -0,0 +1,16 @@
+// console.log(2>1);  true 
+// console.log(5<2);  false
+
+// console.log("02">1);  treats strings as numbers too
+// console.log("5"<2);
+
+// console.log(null>0);  false
+// console.log(null==0); false
+// console.log(null>=0); true
+// == and other comparision work differently 
+// undefined comparisions gives false output
+// null and undefined comparisions are avoided
+
+// === strict check also checks the datatype
+// console.log("02"==2);  true 
+// console.log("02"===2); false
