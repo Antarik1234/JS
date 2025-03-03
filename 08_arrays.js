ca7f0194b017be7381f5fdc7c9a79187f75eba6d
Antarik Sinha <120300062+Antarik1234@users.noreply.github.com>
2025-03-03 14:22:42 +0000

    lec14

--- 08_arrays.js
+++ 08_arrays.js
@@ -1,2 +1,5 @@
 //pop and push for end
 //shift and unshift for beginning
+// .join combines the array and changes to string
+// slice the first in inclusive and the second exclusive , org array intact
+// splice both inclusive , the org array gets changed and the elements are removed 
