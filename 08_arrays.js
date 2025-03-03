573d6c0da8288ed0fda2c95080aa6516aa42e044
Antarik Sinha <120300062+Antarik1234@users.noreply.github.com>
2025-03-03 14:40:06 +0000

    lec15

--- 08_arrays.js
+++ 08_arrays.js
@@ -3,3 +3,6 @@
 // .join combines the array and changes to string
 // slice the first in inclusive and the second exclusive , org array intact
 // splice both inclusive , the org array gets changed and the elements are removed 
+// newArr = [...arr1,...arr2]   // newArr contains all element of arr1 and arr2 like concatination 
+// .flat(infinty) to remove arrays from within an array and spread it
+// Array.from() converts to array used mainly for string
